fc84c81375608674a80398483022335f45ce45c4
Dirk Doesburg <dirk.doesburg@gmail.com>
2020-12-05 17:14:19 +0100

    Add navigator routes to MenuDrawer

--- lib/ui/components/menu_drawer.dart
+++ lib/ui/components/menu_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:reaxit/ui/screens/welcome_screen/welcome_screen.dart';
+import 'package:reaxit/ui/screens/member_list.dart';
 
 class MenuDrawer extends StatelessWidget {
   @override
@@ -10,25 +12,45 @@ class MenuDrawer extends StatelessWidget {
             child: Text('Thalia App'),
           ),
           ListTile(
-              title: Text('Welcome'),
-              leading: Icon(Icons.home),
-              onTap: () => {}),
+            title: Text('Welcome'),
+            leading: Icon(Icons.home),
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => WelcomeScreen()),
+            ),
+          ),
           ListTile(
-              title: Text('Calendar'),
-              leading: Icon(Icons.event),
-              onTap: () => {}),
+            title: Text('Calendar'),
+            leading: Icon(Icons.event),
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => WelcomeScreen()),
+            ),
+          ),
           ListTile(
-              title: Text('Member list'),
-              leading: Icon(Icons.people),
-              onTap: () => {}),
+            title: Text('Member list'),
+            leading: Icon(Icons.people),
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => MemberList()),
+            ),
+          ),
           ListTile(
-              title: Text('Photos'),
-              leading: Icon(Icons.photo),
-              onTap: () => {}),
+            title: Text('Photos'),
+            leading: Icon(Icons.photo),
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => WelcomeScreen()),
+            ),
+          ),
           ListTile(
-              title: Text('Settings'),
-              leading: Icon(Icons.settings),
-              onTap: () => {}),
+            title: Text('Settings'),
+            leading: Icon(Icons.settings),
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => WelcomeScreen()),
+            ),
+          ),
         ],
       ),
     );
