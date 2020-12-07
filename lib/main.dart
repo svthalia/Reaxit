ebbb3e4be8a772ef78631d2540df93d8b4e9c4bc
Dirk Doesburg <dirk.doesburg@gmail.com>
2020-12-07 21:23:04 +0100

    Add theme file

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:reaxit/ui/screens/splash_screen.dart';
+import 'package:reaxit/ui/styles/theme.dart';
 
 import 'model/auth_model.dart';
 
 void main() {
-  runApp(
-      ChangeNotifierProvider<AuthModel>(
-        create: (context) => AuthModel(),
-        child: ThaliApp(),
-      )
-  );
+  runApp(ChangeNotifierProvider<AuthModel>(
+    create: (context) => AuthModel(),
+    child: ThaliApp(),
+  ));
 }
 
 class ThaliApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'ThaliApp',
-        theme: ThemeData(
-          primaryColor: Color(0xFFE62272),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: SplashScreen()
+      title: 'ThaliApp',
+      theme: lightTheme,
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/ui/styles/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  primaryColor: Color(0xFFE62272),
+  accentColor: Color(0xFFE62272),
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
