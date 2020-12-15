423ae1fc03070d281b953fbdb1571d71fa0047e5
Dirk Doesburg <dirk.doesburg@gmail.com>
2020-12-15 11:04:58 +0100

    prettified the error screens, both errors and contents still need to be made fullscreen

--- lib/ui/components/network_scrollable_wrapper.dart
+++ lib/ui/components/network_scrollable_wrapper.dart
@@ -11,45 +11,54 @@ class NetworkScrollableWrapper<T extends ApiService> extends StatefulWidget {
   State<StatefulWidget> createState() => NetWorkScrollableWrapperState<T>();
 }
 
-class NetWorkScrollableWrapperState<T extends ApiService> extends State<NetworkScrollableWrapper<T>> {
+class NetWorkScrollableWrapperState<T extends ApiService>
+    extends State<NetworkScrollableWrapper<T>> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<T>(
-        builder: (context, service, child) {
-          Widget content;
-          switch(service.status) {
-            case ApiStatus.LOADING:
-              content = Center(child: CircularProgressIndicator());
-              break;
-            case ApiStatus.NO_INTERNET:
-              content = _showError('Not connected to internet.');
-              break;
-            case ApiStatus.NOT_AUTHENTICATED:
-              content = _showError('You are not authenticated.');
-              break;
-            case ApiStatus.UNKNOWN_ERROR:
-              content = _showError('An unknown error occurred.');
-              break;
-            default:
-              content = widget.builder(context, service, child);
-          }
-          return RefreshIndicator(
-            onRefresh: () => service.load(),
-            child: SingleChildScrollView(
-              physics: AlwaysScrollableScrollPhysics(),
-              child: content,
-            ),
-          );
-        }
-    );
+    return Consumer<T>(builder: (context, service, child) {
+      Widget content;
+      switch (service.status) {
+        case ApiStatus.LOADING:
+          content = Center(child: CircularProgressIndicator());
+          break;
+        case ApiStatus.NO_INTERNET:
+          content = _showError('Not connected to internet.');
+          break;
+        case ApiStatus.NOT_AUTHENTICATED:
+          content = _showError('You are not authenticated.');
+          break;
+        case ApiStatus.UNKNOWN_ERROR:
+          content = _showError('An unknown error occurred.');
+          break;
+        default:
+          content = widget.builder(context, service, child);
+      }
+      return RefreshIndicator(
+        onRefresh: () => service.load(),
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: content,
+        ),
+      );
+    });
   }
 
   Widget _showError(String message) {
-    return Column(
-      children: [
-        Image.asset('assets/img/sad_cloud.png'),
-        Text(message)
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 150,
+              margin: const EdgeInsets.all(10),
+              child:
+                  Image.asset('assets/img/sad_cloud.png', fit: BoxFit.fitWidth),
+            ),
+            Text(message),
+          ],
+        ),
+      ),
     );
   }
 }
