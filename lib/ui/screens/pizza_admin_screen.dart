54a7276e6ef2a2cea6b1d3118dfa32128eea838a
Dirk Doesburg <dirk.doesburg@gmail.com>
2021-02-19 16:42:34 +0100

    Fix error snackbar, ignore filter in search in PizzaAdminScreen

--- lib/ui/screens/pizza_admin_screen.dart
+++ lib/ui/screens/pizza_admin_screen.dart
@@ -50,65 +50,61 @@ class _PizzaAdminScreenState extends State<PizzaAdminScreen> {
                     itemCount: orderList.length,
                     itemBuilder: (context, index) {
                       PizzaOrder order = orderList[index];
-                      if (filterPaid && order.isPaid) {
-                        return SizedBox(height: 0, width: 0);
-                      } else {
-                        return Column(
-                          children: [
-                            ListTile(
-                              dense: true,
-                              title: Text(order.displayName),
-                              subtitle: Text(order.pizza.name),
-                              trailing: order.payment == "tpay_payment"
-                                  ? DropdownButton(
-                                      items: [
-                                        DropdownMenuItem(
-                                            child: Text("Thalia Pay"))
-                                      ],
-                                      value: order.payment,
-                                    )
-                                  : DropdownButton(
-                                      value: order.payment,
-                                      onChanged: (payment) async {
-                                        try {
-                                          await pizzas.payOrder(order, payment);
-                                          order.payment = payment;
-                                          // payOrder loads the list again, this is only
-                                          // temporary so that the value is updated before
-                                          // finishing the loading.
-                                        } on ApiException {
-                                          Scaffold.of(context).showSnackBar(
-                                            SnackBar(
-                                              content: Text(
-                                                payment == "no_payment"
-                                                    ? "Couldn't mark ${order.displayName}'s order as not paid..."
-                                                    : "Couldn't mark ${order.displayName}'s order as paid...",
-                                              ),
-                                              duration: Duration(seconds: 1),
+                      return Column(
+                        children: [
+                          ListTile(
+                            dense: true,
+                            title: Text(order.displayName),
+                            subtitle: Text(order.pizza.name),
+                            trailing: order.payment == "tpay_payment"
+                                ? DropdownButton(
+                                    items: [
+                                      DropdownMenuItem(
+                                          child: Text("Thalia Pay"))
+                                    ],
+                                    value: order.payment,
+                                  )
+                                : DropdownButton(
+                                    value: order.payment,
+                                    onChanged: (payment) async {
+                                      try {
+                                        await pizzas.payOrder(order, payment);
+                                        order.payment = payment;
+                                        // payOrder loads the list again, this is only
+                                        // temporary so that the value is updated before
+                                        // finishing the loading.
+                                      } on ApiException {
+                                        Scaffold.of(context).showSnackBar(
+                                          SnackBar(
+                                            content: Text(
+                                              (payment == "no_payment")
+                                                  ? "Couldn't mark ${order.displayName}'s order as not paid..."
+                                                  : "Couldn't mark ${order.displayName}'s order as paid...",
                                             ),
-                                          );
-                                        }
-                                      },
-                                      items: [
-                                        DropdownMenuItem(
-                                          value: "no_payment",
-                                          child: Text("Not paid"),
-                                        ),
-                                        DropdownMenuItem(
-                                          value: "cash_payment",
-                                          child: Text("Cash"),
-                                        ),
-                                        DropdownMenuItem(
-                                          value: "card_payment",
-                                          child: Text("Card"),
-                                        ),
-                                      ],
-                                    ),
-                            ),
-                            Divider(),
-                          ],
-                        );
-                      }
+                                            duration: Duration(seconds: 1),
+                                          ),
+                                        );
+                                      }
+                                    },
+                                    items: [
+                                      DropdownMenuItem(
+                                        value: "no_payment",
+                                        child: Text("Not paid"),
+                                      ),
+                                      DropdownMenuItem(
+                                        value: "cash_payment",
+                                        child: Text("Cash"),
+                                      ),
+                                      DropdownMenuItem(
+                                        value: "card_payment",
+                                        child: Text("Card"),
+                                      ),
+                                    ],
+                                  ),
+                          ),
+                          Divider(),
+                        ],
+                      );
                     },
                   );
                 },
@@ -153,7 +149,9 @@ class _PizzaAdminScreenState extends State<PizzaAdminScreen> {
                                   Scaffold.of(context).showSnackBar(
                                     SnackBar(
                                       content: Text(
-                                        "Couldn't mark ${order.displayName}'s order as paid...",
+                                        (payment == "no_payment")
+                                            ? "Couldn't mark ${order.displayName}'s order as not paid..."
+                                            : "Couldn't mark ${order.displayName}'s order as paid...",
                                       ),
                                       duration: Duration(seconds: 1),
                                     ),
