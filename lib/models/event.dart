877372b92b3424d7560dfec22707f7d0d82b7583
Dirk Doesburg <dirk.doesburg@gmail.com>
2021-03-29 22:38:44 +0200

    Add event model

---
+++ lib/models/event.dart
@@ -0,0 +1,59 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'event.g.dart';
+
+enum EventCategory { alumni, education, career, leisure, association, other }
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class Event {
+  final int pk;
+  final String title;
+
+  // TODO: set empty string defaults if necessary:
+  // @JsonKey(defaultValue: '')
+  final String description;
+  final DateTime start;
+  final DateTime end;
+  final EventCategory category;
+  final DateTime? registrationStart;
+  final DateTime? registrationEnd;
+  final DateTime? cancelDeadline;
+  final String location;
+  final double price;
+  final double fine;
+  final int numParticipants;
+  final int maxParticipants;
+  final String noRegistrationMessage;
+  final bool hasFields;
+  final bool isPizzaEvent;
+  final String mapsUrl;
+  final String userPermissions;
+  final String userRegistration;
+  // final Commitee organiser;
+  // final Slide slide;
+
+  factory Event.fromJson(Map<String, dynamic> json) => _$EventFromJson(json);
+
+  Event(
+    this.pk,
+    this.title,
+    this.description,
+    this.start,
+    this.end,
+    this.category,
+    this.registrationStart,
+    this.registrationEnd,
+    this.cancelDeadline,
+    this.location,
+    this.price,
+    this.fine,
+    this.numParticipants,
+    this.maxParticipants,
+    this.noRegistrationMessage,
+    this.hasFields,
+    this.isPizzaEvent,
+    this.mapsUrl,
+    this.userPermissions,
+    this.userRegistration,
+  );
+}
---
+++ lib/models/event.g.dart
@@ -0,0 +1,96 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Event _$EventFromJson(Map<String, dynamic> json) {
+  return Event(
+    json['pk'] as int,
+    json['title'] as String,
+    json['description'] as String,
+    DateTime.parse(json['start'] as String),
+    DateTime.parse(json['end'] as String),
+    _$enumDecode(_$EventCategoryEnumMap, json['category']),
+    json['registration_start'] == null
+        ? null
+        : DateTime.parse(json['registration_start'] as String),
+    json['registration_end'] == null
+        ? null
+        : DateTime.parse(json['registration_end'] as String),
+    json['cancel_deadline'] == null
+        ? null
+        : DateTime.parse(json['cancel_deadline'] as String),
+    json['location'] as String,
+    (json['price'] as num).toDouble(),
+    (json['fine'] as num).toDouble(),
+    json['num_participants'] as int,
+    json['max_participants'] as int,
+    json['no_registration_message'] as String,
+    json['has_fields'] as bool,
+    json['is_pizza_event'] as bool,
+    json['maps_url'] as String,
+    json['user_permissions'] as String,
+    json['user_registration'] as String,
+  );
+}
+
+Map<String, dynamic> _$EventToJson(Event instance) => <String, dynamic>{
+      'pk': instance.pk,
+      'title': instance.title,
+      'description': instance.description,
+      'start': instance.start.toIso8601String(),
+      'end': instance.end.toIso8601String(),
+      'category': _$EventCategoryEnumMap[instance.category],
+      'registration_start': instance.registrationStart?.toIso8601String(),
+      'registration_end': instance.registrationEnd?.toIso8601String(),
+      'cancel_deadline': instance.cancelDeadline?.toIso8601String(),
+      'location': instance.location,
+      'price': instance.price,
+      'fine': instance.fine,
+      'num_participants': instance.numParticipants,
+      'max_participants': instance.maxParticipants,
+      'no_registration_message': instance.noRegistrationMessage,
+      'has_fields': instance.hasFields,
+      'is_pizza_event': instance.isPizzaEvent,
+      'maps_url': instance.mapsUrl,
+      'user_permissions': instance.userPermissions,
+      'user_registration': instance.userRegistration,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$EventCategoryEnumMap = {
+  EventCategory.alumni: 'alumni',
+  EventCategory.education: 'education',
+  EventCategory.career: 'career',
+  EventCategory.leisure: 'leisure',
+  EventCategory.association: 'association',
+  EventCategory.other: 'other',
+};
