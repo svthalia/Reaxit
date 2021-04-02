14594a3b5d892a887db35a088fc9dedee8640782
Dirk Doesburg <dirk.doesburg@gmail.com>
2021-04-02 17:14:04 +0200

    Add ErrorScrollView

---
+++ lib/ui/error_scroll_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ErrorScrollView extends StatelessWidget {
+  final String message;
+
+  const ErrorScrollView(this.message, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const AlwaysScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(20),
+      children: [
+        Container(
+          height: 100,
+          margin: const EdgeInsets.all(10),
+          child: Image.asset(
+            'assets/img/sad_cloud.png',
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Text(message, textAlign: TextAlign.center),
+      ],
+    );
+  }
+}
