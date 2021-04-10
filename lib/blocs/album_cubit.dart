c2c94dbb786f9d7d006355106fcd4b6eba6fde25
Dirk Doesburg <dirk.doesburg@gmail.com>
2021-04-10 19:34:41 +0200

    Add AlbumScreen and AlbumCubit

---
+++ lib/blocs/album_cubit.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reaxit/blocs/api_repository.dart';
+import 'package:reaxit/blocs/detail_state.dart';
+import 'package:reaxit/models/album.dart';
+
+class AlbumCubit extends Cubit<DetailState<Album>> {
+  final ApiRepository api;
+
+  AlbumCubit(this.api) : super(DetailState<Album>.loading());
+
+  void load(int pk) async {
+    emit(state.copyWith(isLoading: true));
+    try {
+      final album = await api.getAlbum(pk: pk);
+      emit(DetailState.result(result: album));
+    } on ApiException catch (exception) {
+      emit(DetailState.failure(message: _failureMessage(exception)));
+    }
+  }
+
+  String _failureMessage(ApiException exception) {
+    switch (exception) {
+      case ApiException.noInternet:
+        return 'Not connected to the internet.';
+      case ApiException.notFound:
+        return 'The album does not exist.';
+      default:
+        return 'An unknown error occurred.';
+    }
+  }
+}
--- lib/ui/screens/album_screen.dart
+++ lib/ui/screens/album_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:reaxit/blocs/album_cubit.dart';
 import 'package:reaxit/models/album.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:reaxit/blocs/api_repository.dart';
+import 'package:reaxit/blocs/detail_state.dart';
+import 'package:reaxit/ui/widgets/error_scroll_view.dart';
 
+/// Screen that loads and shows a the Album of the member with `pk`.
 class AlbumScreen extends StatefulWidget {
   final int pk;
   final ListAlbum? album;
@@ -12,13 +20,99 @@ class AlbumScreen extends StatefulWidget {
 }
 
 class _AlbumScreenState extends State<AlbumScreen> {
+  late final AlbumCubit _cubit;
+
+  @override
+  void initState() {
+    _cubit = AlbumCubit(
+      RepositoryProvider.of<ApiRepository>(context),
+    )..load(widget.pk);
+    super.initState();
+  }
+
+  Widget _makePhotoCard(Album album, int index) {
+    return GestureDetector(
+      onTap: () => _showPhotoGallery(album, index),
+      child: Hero(
+        tag: 'photo_${album.photos[index].pk}',
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/img/default-avatar.jpg',
+          image: album.photos[index].medium,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  void _showPhotoGallery(Album album, int index) {
+    showDialog(
+      context: context,
+      barrierColor: Colors.black.withOpacity(0.9),
+      builder: (context) {
+        return Scaffold(
+          body: Stack(
+            children: [
+              PhotoViewGallery.builder(
+                itemCount: album.photos.length,
+                builder: (context, i) => PhotoViewGalleryPageOptions(
+                  imageProvider: NetworkImage(album.photos[i].full),
+                  tightMode: false,
+                  minScale: PhotoViewComputedScale.contained * 0.8,
+                  maxScale: PhotoViewComputedScale.covered * 2,
+                ),
+                pageController: PageController(initialPage: index),
+              ),
+              // TODO: share button
+              CloseButton(
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+            ],
+          ),
+          backgroundColor: Colors.transparent,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('album ${widget.pk}'),
-      ),
+    return BlocBuilder<AlbumCubit, DetailState<Album>>(
+      bloc: _cubit,
+      builder: (context, state) {
+        if (state.hasException) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.album?.title ?? 'Album'),
+            ),
+            body: ErrorScrollView(state.message!),
+          );
+        } else if (state.isLoading) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.album?.title ?? 'Album'),
+            ),
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(title: Text(state.result!.title)),
+            body: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                crossAxisCount: 3,
+              ),
+              itemCount: state.result!.photos.length,
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding: const EdgeInsets.all(5),
+              itemBuilder: (context, index) => _makePhotoCard(
+                state.result!,
+                index,
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
