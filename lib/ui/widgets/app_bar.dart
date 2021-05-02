4ca8e3f09ed82f92da845e92f3f07e5872f71cc1
Dirk Doesburg <dirk.doesburg@gmail.com>
2021-05-02 15:19:28 +0200

    Add bottom support to AppBar

--- lib/ui/widgets/app_bar.dart
+++ lib/ui/widgets/app_bar.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
 
 class ThaliaAppBar extends AppBar {
   ThaliaAppBar({
     Widget? title,
     List<Widget>? actions,
     Widget? leading,
+    PreferredSizeWidget? bottom,
   }) : super(
           title: title,
           actions: actions,
@@ -16,14 +18,37 @@ class ThaliaAppBar extends AppBar {
           // in dark mode, but is invisible in light mode,
           // so we can just leave it there.
           bottom: PreferredSize(
-            preferredSize: Size.fromHeight(0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(color: Color(0xFFE62272)),
+            preferredSize: bottom?.preferredSize ?? Size.fromHeight(0),
+            child: Column(
+              children: [
+                Container(
+                  height: 0,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Color(0xFFE62272)),
+                    ),
+                  ),
                 ),
-              ),
+                if (bottom != null) bottom,
+              ],
             ),
           ),
         );
 }
+
+// class PreferredSizeWrapper extends PreferredSize {
+//   PreferredSizeWrapper({required PreferredSizeWidget child})
+//       : super(
+//             preferredSize: child.preferredSize,
+//             child: Column(
+//               children: [
+//                 Container(
+//                   decoration: BoxDecoration(
+//                     border: Border(
+//                       bottom: BorderSide(color: Color(0xFFE62272)),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ));
+// }
