86bdb91c002e9d47647e45583ba57efb9439bf2a
Dirk Doesburg <dirk.doesburg@gmail.com>
2021-09-10 22:18:04 +0200

    Replace Heros with OpenContainer animation

--- lib/ui/widgets/member_tile.dart
+++ lib/ui/widgets/member_tile.dart
@@ -1,6 +1,6 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:reaxit/models/member.dart';
-import 'package:reaxit/ui/router.dart';
 import 'package:reaxit/ui/screens/profile_screen.dart';
 
 class MemberTile extends StatelessWidget {
@@ -10,49 +10,44 @@ class MemberTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        ThaliaRouterDelegate.of(context).push(
-          TypedMaterialPage(
-            child: ProfileScreen(pk: member.pk, member: member),
-            name: 'Profile(${member.pk})',
-          ),
-        );
-      },
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Hero(
-            tag: 'member_${member.pk}',
-            child: FadeInImage.assetNetwork(
+    return OpenContainer(
+      routeSettings: RouteSettings(name: 'Profile(${member.pk})'),
+      transitionType: ContainerTransitionType.fadeThrough,
+      closedShape: const RoundedRectangleBorder(),
+      closedBuilder: (context, __) {
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            FadeInImage.assetNetwork(
               placeholder: 'assets/img/default-avatar.jpg',
               image: member.photo.small,
               fit: BoxFit.cover,
               fadeInDuration: const Duration(milliseconds: 200),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.bottomLeft,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              gradient: LinearGradient(
-                begin: FractionalOffset.topCenter,
-                end: FractionalOffset.bottomCenter,
-                colors: [
-                  Colors.black.withOpacity(0.0),
-                  Colors.black.withOpacity(0.5),
-                ],
-                stops: const [0.4, 1.0],
+            Container(
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.bottomLeft,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.0),
+                    Colors.black.withOpacity(0.5),
+                  ],
+                  stops: const [0.4, 1.0],
+                ),
               ),
-            ),
-            child: Text(
-              member.displayName,
-              style: Theme.of(context).primaryTextTheme.bodyText2,
-            ),
-          )
-        ],
-      ),
+              child: Text(
+                member.displayName,
+                style: Theme.of(context).primaryTextTheme.bodyText2,
+              ),
+            )
+          ],
+        );
+      },
+      openBuilder: (_, __) => ProfileScreen(pk: member.pk, member: member),
     );
   }
 }
