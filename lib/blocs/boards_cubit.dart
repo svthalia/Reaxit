76e22b886e421148955d86be3f94cfde30500003
TippieCodes <rowan@tippie.me>
2021-12-15 19:41:22 +0100

    Added cubits

---
+++ lib/blocs/boards_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reaxit/api_repository.dart';
+import 'package:reaxit/blocs/detail_state.dart';
+import 'package:reaxit/models/group.dart';
+
+class BoardsCubit extends Cubit<DetailState<List<ListGroup>>> {
+  final ApiRepository api;
+
+  BoardsCubit(this.api) : super(const DetailState<List<ListGroup>>.loading());
+
+  Future<void> load() async {
+    emit(state.copyWith(isLoading: true));
+    try {
+      final listResponse = await api.getGroups(limit: 1000,offset: 0,type: MemberGroupType.society);
+      if (listResponse.results.isNotEmpty) {
+        emit(DetailState.result(result: listResponse.results));
+      } else {
+        emit(const DetailState.failure(message: 'There are no boards.'));
+      }
+    } on ApiException catch (exception) {
+      emit(DetailState.failure(message: _failureMessage(exception)));
+    }
+  }
+
+  String _failureMessage(ApiException exception) {
+    switch (exception) {
+      case ApiException.noInternet:
+        return 'Not connected to the internet.';
+      default:
+        return 'An unknown error occurred.';
+    }
+  }
+}
