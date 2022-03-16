594a89dc7ff4f3d6a34e07e871e4af2b42355871
Dirk Doesburg <dirk.doesburg@gmail.com>
2022-03-16 18:23:34 +0100

    Update version number to 3.1.1

--- lib/config.dart
+++ lib/config.dart
@@ -50,7 +50,7 @@ const List<String> oauthScopes = [
 
 const Duration searchDebounceTime = Duration(milliseconds: 200);
 
-const String versionNumber = 'v3.1.0';
+const String versionNumber = 'v3.1.1';
 
 final Uri feedbackUri = Uri.parse(
   'https://github.com/svthalia/Reaxit/issues',
