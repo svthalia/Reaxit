ff7b181745a90b56c4851a27608e5fc9c9b6518f
jaap aarts <jaap.aarts1@gmail.com>
2023-02-15 21:25:37 +0100

    Fix messahe for queue cancelation past the deadline
    Fix popup when deregistering after deadline
    
    Revert registration popup
    
    Fix the thing

--- lib/ui/screens/event_screen.dart
+++ lib/ui/screens/event_screen.dart
@@ -221,7 +221,7 @@ class _EventScreenState extends State<EventScreen> {
         registrationButton = _makeCreateRegistrationButton(event);
       }
     } else if (event.canCancelRegistration) {
-      if (event.cancelDeadlinePassed()) {
+      if (event.cancelDeadlinePassed() && event.registration!.isInvited) {
         // Cancel too late message, cancel button with fine warning.
         textSpans.add(TextSpan(
           text: event.cancelTooLateMessage,
@@ -587,7 +587,6 @@ class _EventScreenState extends State<EventScreen> {
               ) ??
               false;
         }
-
         if (confirmed) {
           try {
             final registration = await _eventCubit.register();
