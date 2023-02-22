f6cfc57beaf5df11880bb27104319b9e0c65476d
jaap aarts <jaap.aarts1@gmail.com>
2023-02-22 20:50:12 +0100

    Fix unused import

--- lib/blocs/calendar_cubit.dart
+++ lib/blocs/calendar_cubit.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:reaxit/api/api_repository.dart';
