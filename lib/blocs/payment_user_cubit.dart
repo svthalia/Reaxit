9e1a4875cc855e536edd30313102f67e3455b70d
Joost van Esch <joostvanesch@pm.me>
2023-03-04 17:51:39 +0100

    Changed settled back to false

--- lib/blocs/payment_user_cubit.dart
+++ lib/blocs/payment_user_cubit.dart
@@ -72,8 +72,8 @@ class PaymentUserCubit extends Cubit<PaymentUserState> {
     emit(state.copyWith(isLoading: true));
     try {
       final paymentUser = await api.getPaymentUser();
-      final payments =
-          await api.getPayments(type: [PaymentType.tpayPayment], settled: true);
+      final payments = await api
+          .getPayments(type: [PaymentType.tpayPayment], settled: false);
 
       emit(PaymentUserState.result(
           user: paymentUser, payments: payments.results));
