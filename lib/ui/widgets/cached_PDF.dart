96282d28add69b7518ee60c9b0a41e8cc940f2f0
Marijn Meuleman <marijnjmeuleman@gmail.com>
2023-05-03 20:46:14 +0200

    stash

--- lib/ui/widgets/cached_PDF.dart
+++ lib/ui/widgets/cached_PDF.dart
@@ -17,13 +17,8 @@ class PdfButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-        onPressed: () async {
-          print(path);
-          var test = (await cache.ThaliaCacheManager().getSingleFile(
-                  'https://cdn.staging.thalia.nu/documents/example.pdf?ResponseContentDisposition=attachment%3B+filename%3D%22annual-report-2021.pdf%22&Expires=1682595182&Signature=BQWWndLzObb5xXQUhv-SuJxgPw1aNo8N~Xe0XkZKWjfA1vRNwKxWQOfcDTNsjrjzDM9bsbbsXeqKW6tGtFv4rPBoKJJ8ocTRTMlTC~WzVhJs5VSFL4GBWQPDRPBHaaqZXldT19zZc7d7CK7i-0cqPshZ2FWadlcc4mGfRmSNtI0bdXPNuduLLKa551MiyQRQNf45p7WJCSCC7xNGeIg5iVJPAXib8cDT2GBYcD57eXHrzHrTBkHeQ6DRS5ESiLRGzqD4NnM0iEG~hpqdbmc67Mx0oKdf3PowLnWZuYK86wpaFzbHY5BbobTWhslCRKQOOlW3khkdXb-Aicn-InYgRA__&Key-Pair-Id=K2R1E6JDIG8U40'))
-              .path;
-          print(test);
-          PdfViewer.openFile(test);
+        onPressed: () {
+          cache.ThaliaCacheManager().downloadFile(path);
         },
         icon: const Icon(Icons.pages),
         label: Text(name));
