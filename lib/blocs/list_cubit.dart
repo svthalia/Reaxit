30780ec8f03ef4f172a0c070f8c98c4ebe4dd416
Jaap Aarts <jaap.aarts1@gmail.com>
2023-08-17 14:59:57 +0200

    Add documentation to the abstract ListCubits

--- lib/blocs/list_cubit.dart
+++ lib/blocs/list_cubit.dart
@@ -7,6 +7,10 @@ import 'package:reaxit/config.dart';
 import 'package:reaxit/blocs.dart';
 import 'package:reaxit/models.dart';
 
+// This is an abstract `Cubit`, which needs to be implemented by other classes.
+// If a class implements all the methods of `ListCubit`, it is a Cubit.
+// Since there are a lot of methods to implement, and most `ListCubit`s will be
+// one-way only, users should probably implement `SingleListCubit` instead.
 abstract class ListCubit<T, S> extends Cubit<S> {
   final ApiRepository api;
 
@@ -22,12 +26,15 @@ abstract class ListCubit<T, S> extends Cubit<S> {
 
   ListCubit(this.api, S state) : super(state);
 
+  // Initial load of the data, resets all the state and loads data in both ways.
+  // This follows a couple steps
   Future<void> load() async {
     final query = searchQuery;
     _nextOffsetUp = 0;
     _nextOffsetDown = 0;
     ListResponse<T> upResponse;
     ListResponse<T> downResponse;
+    // Get the data in both directions
     try {
       Future<ListResponse<T>> upResultsFuture = getUp(_nextOffsetUp);
       Future<ListResponse<T>> downResultsFuture = getDown(_nextOffsetDown);
@@ -49,12 +56,15 @@ abstract class ListCubit<T, S> extends Cubit<S> {
     final isDoneUp = _nextOffsetUp == upResponse.count;
     final isDoneDown = _nextOffsetDown == downResponse.count;
 
+    // Do any processing that needs to be done on the data
     List<T> upResults = processUp(upResponse.results);
     List<T> downResults = processDown(downResponse.results);
 
+    // Allow the cubit to move data between up and down
     (upResults: upResults, downResults: downResults) =
         shuffleData(upResults, downResults);
 
+    // Some cubits need to filter out certain results
     upResults = filterUp(upResults);
     downResults = filterDown(downResults);
 
@@ -69,6 +79,9 @@ abstract class ListCubit<T, S> extends Cubit<S> {
     }
   }
 
+  // moreDown loads more data in the down direction. For example when hitting
+  // the bottom when scrolling down. It follows the same prosedure as
+  // initial loading but only does half of it.
   Future<void> moreDown() async {
     final oldState = state;
     final query = searchQuery;
@@ -104,6 +117,9 @@ abstract class ListCubit<T, S> extends Cubit<S> {
     _emit(updateDown(oldState, totalDownResults, isDoneDown));
   }
 
+  // moreUp loads more data in the up direction. For example when hitting
+  // the top when scrolling up. It follows the same prosedure as
+  // initial loading but only does half of it.
   Future<void> moreUp() async {
     final oldState = state;
     final query = searchQuery;
@@ -156,51 +172,91 @@ abstract class ListCubit<T, S> extends Cubit<S> {
   }
 
   void _emit(S state) {
+    // Since functions cannot be interupted in dart(without await), and isolates
+    // dont share memory. Thus we know there is no interuption between this check
+    // and emiting the state
     if (isClosed) {
       return;
     }
     emit(state);
   }
 
+  // getUp returns a future with more data in the up direction.
+  // This is where you would want to do an http request for example.
   Future<ListResponse<T>> getUp(int offset);
+  // getDown returns a future with more data in the down direction
+  // This is where you would want to do an http request for example.
   Future<ListResponse<T>> getDown(int offset);
 
   // This is called when a new load is triggered (after the API is called)
   void cleanupOldState() => {};
 
+  // processUp is called after getUp, and is used to process the data.
+  // For example, this can split daturned calendar events into multiple events.
   List<T> processUp(List<T> upResults) => upResults;
+  // processDown is called after getDown, and is used to process the data.
+  // For example, this can split daturned calendar events into multiple events.
   List<T> processDown(List<T> downResults) => downResults;
 
-  // This is supposed to resolve some issues around the up/down boundry.
-  // For example, when some results from up should be moved to down, this can be overwritten.
+  // This is supposed to resolve issues around the up/down boundry.
+  // For example, when some results from up should be moved to down,
+  // this can be overwritten.
   ({List<T> upResults, List<T> downResults}) shuffleData(
           List<T> upResults, List<T> downResults) =>
       (upResults: upResults, downResults: downResults);
 
+  // filterUp filters results in the up direction.
+  // This can be used to remove data that should not (yet) be shown to
+  // the user.
   List<T> filterUp(List<T> upResults) => upResults;
+  // filterDown filters results in the down direction.
+  // This can be used to remove data that should not (yet) be shown to
+  // the user.
   List<T> filterDown(List<T> downResults) => downResults;
 
+  // combineUp merges the new data with the new data, for the final result
   List<T> combineUp(List<T> upResults, S oldstate);
+  // combineDown merges the new data with the new data, for the final result
   List<T> combineDown(List<T> downResults, S oldstate);
 
+  // supressMoreUp is called to check if more data can/should be loaded
+  // (in loadMoreUp). For example when there is no more data, or we are
+  // already loading more data.
   bool supressMoreUp(S oldstate);
+  // supressMoreDown is called to check if more data can/should be loaded
+  // (in loadMoreDown). For example when there is no more data, or we are
+  // already loading more data.
   bool supressMoreDown(S oldstate);
 
+  // loading returns a state to be used when loading fully new data
   S loading();
+  // loadingUp returns a state to be used when loading new data in the up direction
   S loadingUp(S oldstate);
+  // loadingDown returns a state to be used when loading new data in the down direction
   S loadingDown(S oldstate);
+  // empty returns a state to be used when showing there is no data
   S empty(String query);
+  // failure returns a state to be used when showing there was a failure
   S failure(String message);
+  // newState returns a fully new state to be used in load
   S newState({
     List<T> resultsUp = const [],
     List<T> resultsDown = const [],
     required bool isDoneUp,
     required bool isDoneDown,
   });
+  // updateUp returns a state to be used when there is new data in the
+  // up direction
   S updateUp(S oldstate, List<T> upResults, bool isDoneUp);
+  // updateDown returns a state to be used when there is new data in the
+  // down direction
   S updateDown(S oldstate, List<T> downResults, bool isDoneDown);
 }
 
+// This class should be implemented when implementing a listcubit when only
+// needing to show data on one side (usually down). The methods you need to
+// overwrite are `getDown`, `combineDown`, and `empty`. This should makes it
+// trivial to implement a single ended ListCubit.
 abstract class SingleListCubit<T> extends ListCubit<T, ListState<T>> {
   SingleListCubit(ApiRepository api)
       : super(api, const ListState.loading(results: []));
