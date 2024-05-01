1ef06458d3cb101d28aacbb77521083f60068ca6
Jaap Aarts <jaap.aarts1@gmail.com>
2024-05-01 21:57:30 +0200

    Add sleep

--- integration_test/main.dart
+++ integration_test/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:integration_test/integration_test.dart';
 import 'album_screen_test.dart';
 import 'calendar_screen_test.dart';
@@ -7,6 +9,7 @@ void main() async {
   final IntegrationTestWidgetsFlutterBinding binding =
       IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  sleep(const Duration(seconds: 1));
   testAlbum(binding);
   testCallender();
   testLogin();
