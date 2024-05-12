d36e7a9e72e91c6209cdfef6cbdd47aa29fe37ee
Jaap Aarts <jaap.aarts1@gmail.com>
2024-05-12 13:13:53 +0200

    Remove all mocks

--- test/mocks.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:reaxit/api/api_repository.dart';
-import 'package:reaxit/blocs.dart';
-
-@GenerateMocks([
-  AuthCubit,
-  ApiRepository,
-])
-void main() {}
