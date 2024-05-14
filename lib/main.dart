5eda1c8d2772989674179757ee0a185a6dfd43dd
Jaap Aarts <jaap.aarts1@gmail.com>
2024-05-15 00:24:06 +0200

    Remove intermediary widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,20 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 Future<void> testingMain() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Google Fonts doesn't need to download fonts as they are bundled.
-  GoogleFonts.config.allowRuntimeFetching = false;
-
-  // Add licenses for the used fonts.
-  LicenseRegistry.addLicense(() async* {
-    final openSansLicense = await rootBundle.loadString(
-      'assets/google_fonts/OpenSans-OFL.txt',
-    );
-    final oswaldLicense = await rootBundle.loadString(
-      'assets/google_fonts/Oswald-OFL.txt',
-    );
-    yield LicenseEntryWithLineBreaks(['google_fonts'], openSansLicense);
-    yield LicenseEntryWithLineBreaks(['google_fonts'], oswaldLicense);
-  });
-
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(
-    const ThaliApp(),
-  );
-}
-
-class ThaliApp extends StatefulWidget {
-  const ThaliApp();
-
-  @override
-  State<ThaliApp> createState() => _ThaliAppState();
-}
-
-class _ThaliAppState extends State<ThaliApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+    MaterialApp(
       home: FadeInImage.assetNetwork(
         placeholder: 'assets/img/photo_placeholder_0.png',
         image:
             'https://raw.githubusercontent.com/svthalia/Reaxit/3e3a74364f10cd8de14ac1f74de8a05aa6d00b28/assets/img/default-avatar.jpg',
         fit: BoxFit.cover,
       ),
-    );
-  }
+    ),
+  );
 }
