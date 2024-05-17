fe7c99c0852a7cbf8e44bbb73647ac473baa090d
Jaap Aarts <jaap.aarts1@gmail.com>
2024-05-17 11:35:00 +0200

    Remove screenshot code

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,20 +1,9 @@
-import 'dart:io';
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
   try {
-    await integrationDriver(
-      onScreenshot: saveScreenshot,
-    );
+    await integrationDriver();
   } catch (e) {
     throw ('Error occured: $e');
   }
-}
-
-Future<bool> saveScreenshot(String screenshotName, List<int> screenshotBytes,
-    [Map<String, Object?>? args]) async {
-  final File image =
-      await File('screenshots/$screenshotName.png').create(recursive: true);
-  image.writeAsBytesSync(screenshotBytes);
-  return true;
 }
